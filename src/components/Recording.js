cf880cc867ef3bc72754fa66e3ffa81d9982359b
mansurissa <mansurissa6@gmail.com>
2022-02-26 20:18:29 +0100

    assking mic permision on app loads

--- src/components/Recording.js
+++ src/components/Recording.js
@@ -4,8 +4,8 @@ import React from 'react'
 function Recording(props) {
   return (
     <div>
-        <div className='pulse-ring'></div>
-        <i className='fa  fa-microphone icon z-1'></i>
+        <div className='pulse-ring'> <i className='fa  fa-microphone icon z-1'></i></div>
+
     </div>
   )
 }
